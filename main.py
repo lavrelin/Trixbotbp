6192b1c155c7c7cc6c5f60442fd47880a7d9c06e
lavrelin <pwnzartemlavrelin@gmail.com>
2025-08-10 15:33:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -93,14 +93,13 @@ def main():
     application.add_handler(CommandHandler("scheduler_off", scheduler_handler.scheduler_off))
     application.add_handler(CommandHandler("scheduler_message", scheduler_handler.scheduler_message))
     application.add_handler(CommandHandler("scheduler_test", 
-    # Callback query handlers
-application.add_handler(CallbackQueryHandler(start_handler.handle_registration_callback, pattern="^reg:"))
-application.add_handler(CallbackQueryHandler(menu_handler.handle_menu_callback, pattern="^
-    application.add_handler(CallbackQueryHandler(menu_handler.handle_menu_callback, pattern="^menu:"))
-    application.add_handler(CallbackQueryHandler(publication_handler.handle_publication_callback, pattern="^pub:"))
-    application.add_handler(CallbackQueryHandler(piar_handler.handle_piar_callback, pattern="^piar:"))
-    application.add_handler(CallbackQueryHandler(moderation_handler.handle_moderation_callback, pattern="^mod:"))
-    application.add_handler(CallbackQueryHandler(profile_handler.handle_profile_callback, pattern="^profile:"))
+   
+# Callback query handlers
+application.add_handler(CallbackQueryHandler(menu_handler.handle_menu_callback, pattern="^menu:"))
+application.add_handler(CallbackQueryHandler(publication_handler.handle_publication_callback, pattern="^pub:"))
+application.add_handler(CallbackQueryHandler(piar_handler.handle_piar_callback, pattern="^piar:"))
+application.add_handler(CallbackQueryHandler(moderation_handler.handle_moderation_callback, pattern="^mod:"))
+application.add_handler(CallbackQueryHandler(profile_handler.handle_profile_callback, pattern="^profile:"))
     
     # Message handlers
     application.add_handler(MessageHandler(
