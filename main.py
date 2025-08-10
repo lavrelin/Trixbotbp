330aed1c86cd4e3f0dd1d64ddf67f04cfbcf5a8a
lavrelin <pwnzartemlavrelin@gmail.com>
2025-08-10 15:29:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -92,9 +92,10 @@ def main():
     application.add_handler(CommandHandler("scheduler_on", scheduler_handler.scheduler_on))
     application.add_handler(CommandHandler("scheduler_off", scheduler_handler.scheduler_off))
     application.add_handler(CommandHandler("scheduler_message", scheduler_handler.scheduler_message))
-    application.add_handler(CommandHandler("scheduler_test", scheduler_handler.scheduler_test))
-    
+    application.add_handler(CommandHandler("scheduler_test", 
     # Callback query handlers
+application.add_handler(CallbackQueryHandler(start_handler.handle_registration_callback, pattern="^reg:"))
+application.add_handler(CallbackQueryHandler(menu_handler.handle_menu_callback, pattern="^
     application.add_handler(CallbackQueryHandler(menu_handler.handle_menu_callback, pattern="^menu:"))
     application.add_handler(CallbackQueryHandler(publication_handler.handle_publication_callback, pattern="^pub:"))
     application.add_handler(CallbackQueryHandler(piar_handler.handle_piar_callback, pattern="^piar:"))
