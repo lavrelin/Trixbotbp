373558d062979f8043ca5bac3d86297e6ff19c10
lavrelin <pwnzartemlavrelin@gmail.com>
2025-08-10 02:07:58 +0200

    Add files via upload

---
+++ trix_handlers_init.py
@@ -0,0 +1,22 @@
+# Export all handlers
+from . import (
+    start_handler,
+    menu_handler,
+    publication_handler,
+    piar_handler,
+    moderation_handler,
+    admin_handler,
+    profile_handler,
+    scheduler_handler
+)
+
+__all__ = [
+    'start_handler',
+    'menu_handler',
+    'publication_handler',
+    'piar_handler',
+    'moderation_handler',
+    'admin_handler',
+    'profile_handler',
+    'scheduler_handler'
+]
