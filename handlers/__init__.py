74003260d2db1d4a4b276a1d70f4fd4d07381e48
lavrelin <pwnzartemlavrelin@gmail.com>
2025-09-14 16:38:58 +0200

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,22 +1,15 @@
-# Export all handlers
-from . import (
-    start_handler,
-    menu_handler,
-    publication_handler,
-    piar_handler,
-    moderation_handler,
-    admin_handler,
-    profile_handler,
-    scheduler_handler
-)
+# handlers/__init__.py
+
+# This file makes the handlers directory a Python package
+# All handler modules are imported in main.py as needed
 
 __all__ = [
     'start_handler',
-    'menu_handler',
+    'menu_handler', 
     'publication_handler',
     'piar_handler',
+    'profile_handler',
     'moderation_handler',
     'admin_handler',
-    'profile_handler',
     'scheduler_handler'
 ]
